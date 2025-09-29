b2b80b6d4240a0fe36e15d30778cee7874ec458f
Hsiangting <stpeng777@gmail.com>
2025-09-29 16:55:14 -0700

    implemented wave_03

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -11,10 +11,24 @@ class Vendor:
             return False
         self.inventory.remove(item)
         return item
-    
+
     def get_by_id(self, number):
         for item in self.inventory:
             if item.id == number:
                 return item
         else:
-            return None 
+            return None
+
+    def swap_items(self, other_vendor, my_item, their_item):
+        my_item_exists = my_item in self.inventory
+        their_item_exists = their_item in other_vendor.inventory
+        if not my_item_exists or not their_item_exists:
+            return False
+
+        self.remove(my_item)
+        other_vendor.add(my_item)
+
+        other_vendor.remove(their_item)
+        self.add(their_item)
+
+        return True
