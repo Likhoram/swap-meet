e2873191d70b58594b749df5eec3316f6e4e689e
Hsiangting <stpeng777@gmail.com>
2025-09-30 21:43:23 -0700

    implemented clothing.py , electronocs.py and passed tests

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,16 +1,18 @@
-import uuid
+# import uuid
+from swap_meet.item import Item
 
-class Clothing:
+class Clothing(Item):
     def __init__(self, id=None, fabric="Unknown", condition=0):
-        self.id = id or uuid.uuid4().int
+        super().__init__(id, condition)
+        # self.id = id or uuid.uuid4().int
         self.fabric = fabric
-        self.condition = condition
+        # self.condition = condition
 
     def get_category(self):
         return "Clothing"
-    
+
     def __str__(self):
         return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
-    
-    def condition_description(self):
-        return f"the value of condition is {self.condition}."
+
+    # def condition_description(self):
+    #     return f"the value of condition is {self.condition}."
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,16 +1,18 @@
 import uuid
+from swap_meet.item import Item
 
-class Electronics:
+class Electronics(Item):
     def __init__(self, id=None, type="Unknown", condition=0):
-        self.id = id or uuid.uuid4().int
+        # self.id = id or uuid.uuid4().int
+        super().__init__(id, condition)
         self.type = type
-        self.condition = condition
+        # self.condition = condition
 
     def get_category(self):
         return "Electronics"
-    
+
     def __str__(self):
         return f"An object of type Electronics with id {self.id}. This is a {self.type} device."
 
-    def condition_description(self):
-        return f"the value of condition is {self.condition}."
+    # def condition_description(self):
+    #     return f"the value of condition is {self.condition}."
